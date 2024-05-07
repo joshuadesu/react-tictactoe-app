bea5266b9c7b6f5c024e6406ccc14d4195d2b377
joshuadesu <meron_pan@kakao.com>
2024-05-07 10:43:51 +0900

    Feat: Add Board 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Board from './components/Board.js';
 
 function App() {
   return (
     <div className="game">
       <div className="game-board">
+        <Board />
       </div>
       <div className="game-info">
       </div>
