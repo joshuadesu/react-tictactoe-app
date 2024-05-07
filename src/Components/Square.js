64e0a86b48ab101fab7cb85a96808f0cb893426d
joshuadesu <meron_pan@kakao.com>
2024-05-07 11:49:56 +0900

    Feat: State 추가

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import './Square.css'
 
 export default class Square extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null, 
+    }
+  }
+
   render() {
     return(
-      <button className='square'>
+      <button className='square' onClick={() => { this.setState(value: 'X' )})}>
         {this.props.value}
       </button>
     )
