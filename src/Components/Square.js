4bc606d13523de1cb7ee5f2cf32820a88c7fa7b7
joshuadesu <meron_pan@kakao.com>
2024-05-07 10:30:47 +0900

    Feat: Add Square Component

---
+++ src/Components/Square.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Square extends React.Component {
+  render() {
+    return(
+      <button className='square'>
+        Square
+      </button>
+    )
+  }
+}
